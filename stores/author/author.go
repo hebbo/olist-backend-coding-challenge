02807db1699204dd431baeebec35d849fd09d56b
Angel Pena Ruiz <angel@cultureamp.com>
2021-10-19 20:15:09 +1000

    Add author store

--- stores/author/author.go
+++ stores/author/author.go
@@ -2,15 +2,23 @@ package author
 
 import "errors"
 
-type author struct {
+type Store struct {
+	Data map[int]Author
+}
+
+type Author struct {
 	id   int64
 	name string
 }
 
-func Add(id int64, name string) error {
+func Add(s Store, id int64, name string) (Store, error) {
 	if len(name) == 0 {
-		return errors.New("incorrect name")
+		return s, errors.New("incorrect name")
 	}
 
-	return nil
+	return Store{
+		Data: map[int]Author{
+			1: {id: id, name: name},
+		},
+	}, nil
 }
--- stores/author/author_test.go
+++ stores/author/author_test.go
@@ -6,13 +6,19 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	err := author.Add(1, "lorem ipsum")
+	empty_store := author.Store{}
+
+	new_store, err := author.Add(empty_store, 1, "lorem ipsum")
 
 	if err != nil {
 		t.Errorf("unable to add user")
 	}
 
-	err = author.Add(1, "")
+	if new_store.Data == nil {
+		t.Errorf("incorrect store returned")
+	}
+
+	_, err = author.Add(empty_store, 1, "")
 
 	if err == nil {
 		t.Errorf("author name can't be empty")
